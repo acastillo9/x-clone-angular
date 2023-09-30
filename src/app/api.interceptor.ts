624b721cfecb5084f27bda8285b1f41dbf2e35bb
Andrés Castillo <info@acastillo.dev>
2023-09-29 19:25:17 -0500

    fix deploy error in netlify

--- src/app/api.interceptor.ts
+++ src/app/api.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://my-json-server.typicode.com/acastillo9/x-clone-angular'
+    apiUrl: 'http://localhost:3000'
 };
